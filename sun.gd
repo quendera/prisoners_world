30c4c027ce441a5e0ceda30a676dca35162be4f4
Asagodi <abel.sagodi@gmail.com>
2022-02-24 15:41:50 +0000

    world goes round

--- sun.gd
+++ sun.gd
@@ -1,16 +1,9 @@
 extends DirectionalLight
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	#world_goes_round = true
+	var seconds = 10*60
+	for i in range(seconds):
+		self.rotate_y(PI/float(seconds))
+		yield(get_tree().create_timer(1), "timeout")
